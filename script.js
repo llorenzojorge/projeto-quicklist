f0b423f3fcbfbd3e8c1fd7087c7e17c1af6ee64e
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2025-10-13 20:41:31 -0300

    function createNewItem

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ const itemsList = document.getElementById("items-list")
 const itemDefault = itemsList.querySelector("li")
 const addItem = document.getElementById("add-item")
 
-// Clonando a <li> inteira do items-list.
-const newItem = itemDefault.cloneNode(true)
-
 form.onsubmit = (event) => {
   event.preventDefault()
   
   // Armazenando o dado informado pelo usuário
-  // const itemValue = addItem.value
-  // newItem.querySelector("span").textContent = `${itemValue}`
- 
+  const itemValue = addItem.value
+  createNewItem(itemValue) 
+}
+
+function createNewItem(itemValue) {
+  // Clonando a <li> inteira do items-list.
+  const newItem = itemDefault.cloneNode(true)
+
+  // Alterando o span da <li> clonada e adicionando o valor definido pelo user. 
+  newItem.querySelector("span").textContent = `${itemValue}`
+  return itemsList.appendChild(newItem)
 }
