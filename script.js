4084a834f02525e0bfdd56141f5b630924fe8293
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2025-10-13 20:56:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const form = document.getElementById("add-item-form")
 const itemsList = document.getElementById("items-list")
 const itemDefault = itemsList.querySelector("li")
 const addItem = document.getElementById("add-item")
+const trash = document.querySelector(".delete-item")
+// const trash = document.getElementsByClassName("delete-item")
 
 form.onsubmit = (event) => {
   event.preventDefault()
@@ -20,3 +22,9 @@ function createNewItem(itemValue) {
   newItem.querySelector("span").textContent = `${itemValue}`
   return itemsList.appendChild(newItem)
 }
+
+// CRIAR FUNÇÃO/EVENTO PARA REMOVER LI AO CLICAR NO LIXO
+// FAZER APARECER A MENSAGEM DE EXCLUSÃO
+trash.addEventListener("click", () => {
+  confirm("Deseja excluir o item selecionado?")
+})
