3d25045ceacbff6878514364f9fdc0c5a6860c9e
Lorenzo <lorenzojorge932@gmail.com>
2025-10-13 17:14:17 -0300

    Iniciando script

---
+++ script.js
@@ -0,0 +1,6 @@
+// Armazenando dados do html.
+const form = document.getElementById("add-item-form")
+const itemsList = document.getElementById("items-list")
+const itemDefault = itemsList.querySelector("li")
+const addItem = document.getElementById("add-item")
+
